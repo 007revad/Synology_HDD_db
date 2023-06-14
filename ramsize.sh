478654156c2ec9ebc858f8c304da6ecafcd2a9ff
Dave Russell <39733752+007revad@users.noreply.github.com>
2023-06-14 19:55:44 +1000

    Update ramsize.sh

--- ramsize.sh
+++ ramsize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #IFS=$'\n' read -r -d '' -a array < <(dmidecode -t memory | grep "[Ss]ize")
-#for f in "${!array[@]}"; do echo "'${array[f]}'";  ramsize=$(printf %s "${array[num]}" | awk '{print $2}'); echo "'$ramsize'"; done
+#for f in "${!array[@]}"; do echo "'${array[f]}'";  r=$(printf %s "${array[num]}" | awk '{print $2}'); echo "'$r'"; done
 
 IFS=$'\n' read -r -d '' -a array < <(dmidecode -t memory | grep "[Ss]ize")
 if [[ ${#array[@]} -gt "0" ]]; then
