f58eb38ad16090480908f4ed2726eef7403df802
Dave Russell <39733752+007revad@users.noreply.github.com>
2023-06-14 19:40:24 +1000

    Create ramsize.sh

---
+++ ramsize.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+IFS=$'\n' read -r -d '' -a array < <(dmidecode -t memory | grep "[Ss]ize")
+if [[ ${#array[@]} -gt "0" ]]; then
+    for f in "${!array[@]}"; do
+        echo "'${array[f]}'"
+        ramsize=$(printf %s "${array[num]}" | awk '{print $2}')
+        echo "'$ramsize'"
+    done
+fi
+
+exit
+
