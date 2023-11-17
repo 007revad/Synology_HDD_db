b9a0379afe0849f159b1beb43860a2f6b5728dd1
Dave Russell <39733752+007revad@users.noreply.github.com>
2023-11-17 18:55:19 +1100

    Update syno_hdd_db.sh

--- syno_hdd_db.sh
+++ syno_hdd_db.sh
@@ -731,13 +731,7 @@ fixdrivemodel(){
         hdmodel=$(printf "%s" "$1" | sed 's/ 00Y.*//')
     fi
     if [[ $1 =~ ^[A-Za-z]{1,7}" ".* ]]; then
-        hdmodel=${hdmodel#"WDC "}       # Remove "WDC " from start of model name
-        hdmodel=${hdmodel#"HGST "}      # Remove "HGST " from start of model name
-        hdmodel=${hdmodel#"TOSHIBA "}   # Remove "TOSHIBA " from start of model name
-        hdmodel=${hdmodel#"Hitachi "}   # Remove "Hitachi " from start of model name
-        hdmodel=${hdmodel#"SAMSUNG "}   # Remove "SAMSUNG " from start of model name
-        hdmodel=${hdmodel#"FUJISTU "}   # Remove "FUJISTU " from start of model name
-        hdmodel=${hdmodel#"APPLE HDD "} # Remove "APPLE HDD " from start of model name
+	echo
     fi
 }
 
