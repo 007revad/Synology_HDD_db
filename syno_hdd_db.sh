ab8a84b4b997ce3b9efb96e073af72e72fc1aff2
Dave Russell <39733752+007revad@users.noreply.github.com>
2023-11-17 18:45:19 +1100

    Update syno_hdd_db.sh

--- syno_hdd_db.sh
+++ syno_hdd_db.sh
@@ -727,12 +727,10 @@ fi
 # PCIe M.2 cards and connected Expansion Units.
 
 fixdrivemodel(){ 
-    # Remove " 00Y" from end of Samsung/Lenovo SSDs  # Github issue #13
     if [[ $1 =~ MZ.*" 00Y" ]]; then
         hdmodel=$(printf "%s" "$1" | sed 's/ 00Y.*//')
     fi
 
-    # Brands that return "BRAND <model>" and need "BRAND " removed.
     if [[ $1 =~ ^[A-Za-z]{1,7}" ".* ]]; then
         # See  Smartmontools database in /var/lib/smartmontools/drivedb.db
         hdmodel=${hdmodel#"WDC "}       # Remove "WDC " from start of model name
