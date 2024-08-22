3aff2b3ea6e1b161348f592e336e9fe047645d88
Dave Russell <39733752+007revad@users.noreply.github.com>
2024-08-22 16:52:19 +1000

    Update syno_hdd_db.sh

--- syno_hdd_db.sh
+++ syno_hdd_db.sh
@@ -29,7 +29,7 @@
 # /var/packages/StorageManager/target/ui/storage_panel.js
 
 
-scriptver="v3.5.100"
+scriptver="v3.5.101"
 script=Synology_HDD_db
 repo="007revad/Synology_HDD_db"
 scriptname=syno_hdd_db
